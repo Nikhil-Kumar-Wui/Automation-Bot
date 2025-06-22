b6586c701a009e6f77ea43020bc095de8eaba9e5
Nikhil Kumar <nikhilkumarnkh12@gmail.com>
2025-06-22 10:28:29 +0530

    Update automation-bot.py

--- automation-bot.py
+++ automation-bot.py
@@ -1,7 +1,7 @@
 # Necessary imports
 import streamlit as st
 import os
-
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import textwrap
 import fitz
