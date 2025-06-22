18a33eddcd78925b8db40c4ef30e488ebe015426
Nikhil Kumar <nikhilkumarnkh12@gmail.com>
2025-06-22 10:12:05 +0530

    Update context-aware-bot.py

--- context-aware-bot.py
+++ context-aware-bot.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import textwrap
 import fitz
